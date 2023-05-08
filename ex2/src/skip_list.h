fa5632589d58a49d1ff81987fa74e18acfcbd68e
Federico Filì <federico.fili@di.unito.it>
2023-05-08 19:04:40 +0200

    aggiunta libreria di skiplist

--- ex2/src/skip_list.h
+++ ex2/src/skip_list.h
@@ -2,8 +2,20 @@
 #ifndef _SKIP_LIST_H
 #define _SKIP_LIST_H
 
-#define MAX_HEIGHT 20
+#define MAX_HEIGHT 21
 
+
+struct SkipList;
+struct Node;
+
+/**
+ * @brief  La SkipList è un tipo di lista concatenata che memorizza una lista ordinata di elementi
+ * 
+ * @param head è il primo nodo della SkipList
+ * @param max_level è il numero massimo di puntatori che al momento ci sono in un singolo nodo della SkipList
+ * @param max_height è una costante che definisce il massimo numero di puntatori che possono esserci in un singolo nodo della SkipList
+ * @param compare è il criterio secondo cui ordinare i dati
+ */
 struct SkipList {
   struct Node *head;
   size_t max_level;
@@ -11,21 +23,51 @@ struct SkipList {
   int (*compare)(const void*, const void*);
 };
 
+/**
+ * @brief Ogni Node contiene un puntatore all'elemento successivo nella lista
+ * 
+ * @param next è l'array di puntatori in un dato nodo della SkipList
+ * @param size è il mumero di puntatori in un dato nodo della SkipList 
+ * @param item è il dato memorizzato in un dato nodo della SkipList
+ */
 struct Node {
   struct Node **next;
   size_t size;
   void *item;
 };
 
+/**
+ * @brief 
+ * 
+ * @param list 
+ * @param max_height 
+ * @param compar 
+ */
+void new_skiplist(struct SkipList **list, size_t max_height, int (*compar)(const void *, const void*));
 
+/**
+ * @brief 
+ * 
+ * @param list 
+ */
+void clear_skiplist(struct SkipList **list);
 
+/**
+ * @brief 
+ * 
+ * @param list 
+ * @param item 
+ */
+void insert_skiplist(struct SkipList *list, void *item);
 
-
-
-
-
-
-
+/**
+ * @brief 
+ * 
+ * @param list 
+ * @param item 
+ * @return const void* 
+ */
+const void* search_skiplist(struct SkipList *list, void *item);
 
 
 #endif
