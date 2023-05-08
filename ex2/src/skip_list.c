7891eb92137f880cb1736e82cf18d71a387a219c
Federico Filì <federico.fili@di.unito.it>
2023-05-08 19:04:08 +0200

    funzioni di creazione della skiplist

--- ex2/src/skip_list.c
+++ ex2/src/skip_list.c
@@ -1,5 +1,52 @@
-#include "skip_list.h"
-
 #include <stdlib.h>
 #include <string.h>
+
+#include "skip_list.h"
+ 
+
+ int random_level() {
+  int level = 1;
+  double temp = (double)rand() / (double)RAND_MAX;
+  while (temp < 0.5 && level < MAX_HEIGHT) {
+    level += 1;
+    temp = (double)rand() / (double)RAND_MAX;
+  }
+  return level;
+}
+
+void new_skiplist(struct SkipList **list, size_t max_height, int (*compar)(const void *, const void*)) {
+    struct SkipList *new_skip_list = (struct SkipList *) malloc(sizeof(struct SkipList));
+    *list = new_skip_list;
+    new_skip_list->max_level = 0;
+    new_skip_list->head = malloc(sizeof(struct Node));
+    new_skip_list->compare = compar;
+    new_skip_list->max_height = max_height;
+    new_skip_list->head->next = calloc(sizeof(struct Node), max_height);
+    printf("Successo: SkipList creata\n");
+}
+
+void clear_skiplist(struct SkipList **list) {
+    if(list == NULL || *list == NULL) 
+        return;
+    if((*list)->head != NULL) {
+        if(((*list)->head->item) != NULL) {
+            free((*list)->head->item);
+        }
+    }
+    free(*list);
+    *list = NULL;
+}
+
+
+
+struct Node *createNode(void *item, int level) {
+    struct Node *new_node = malloc(sizeof(struct Node));
+    new_node->item = malloc(sizeof(char *));
+    memcpy(new_node->item, item, sizeof(char *));
+    new_node->next = calloc(sizeof(struct Node), level);
+    new_node->size = level;
+    return new_node;
+}
+
+
 
