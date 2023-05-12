b5778d18475ba1182abb168d9dd12b0330b5db78
Federico Filì <federico.fili@di.unito.it>
2023-05-12 16:38:46 +0200

    aggiunte nuove funzioni

--- ex3-4/src/PriorityQueue/AbstractQueue.java
+++ ex3-4/src/PriorityQueue/AbstractQueue.java
@@ -1,9 +1,11 @@
 package PriorityQueue;
 
 public interface AbstractQueue<E> {
-    public boolean empty(); // controlla se la coda è vuota
-    public boolean push(E e); // aggiunge un elemento alla coda
-    public E top(); // accede all'elemento in cima alla coda
-    public void pop(); // rimuove l'elemento in cima alla coda
+    public boolean empty(); // controlla se la coda è vuota -- O(1)
+    public boolean push(E e); // aggiunge un elemento alla coda -- O(logN)
+    public boolean contains(E e); // controlla se un elemento è in coda -- O(1)
+    public E top(); // accede all'elemento in cima alla coda -- O(1)
+    public void pop(); // rimuove l'elemento in cima alla coda -- O(logN)
+    public boolean remove(E e); // rimuove un elemento se presente in coda -- O(logN)
 };
 
