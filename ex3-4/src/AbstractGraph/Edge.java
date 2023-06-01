76537698f61c4b9c0951798a2182f06439ccbdc9
Federico Filì <federico.fili@di.unito.it>
2023-06-01 13:31:20 +0200

    fix classe Edge

--- ex3-4/src/AbstractGraph/Edge.java
+++ ex3-4/src/AbstractGraph/Edge.java
@@ -1,7 +1,6 @@
 package AbstractGraph;
 
-public class Edge<V, L extends Comparable<L>> implements AbstractEdge<V, L>, Comparable<Edge<V, L>> {
-
+public class Edge<V, L> implements AbstractEdge<V, L> {
     private V start;
     private V end;
     private L label;
@@ -12,31 +11,15 @@ public class Edge<V, L extends Comparable<L>> implements AbstractEdge<V, L>, Com
         this.label = label;
     }
 
-    @Override
     public V getStart() {
         return start;
     }
 
-    @Override
     public V getEnd() {
         return end;
     }
 
-    @Override
     public L getLabel() {
         return label;
-    }
-
-    @Override
-    public int compareTo(Edge<V, L> o) {
-        if(o == null){
-            return 1;
-        }
-        return label.compareTo(o.getLabel());
-    }
-
-    @Override
-    public String toString(){
-        return start + "," + end + "," + label;
     }
 }
