3b530d215fc29974c71b8fb2e9fe841d8b76c886
Federico Filì <federico.fili@di.unito.it>
2023-06-02 17:45:35 +0200

    implementazione della classe Prim

---
+++ ex3-4/src/AbstractGraph/Prim.java
@@ -0,0 +1,14 @@
+package AbstractGraph;
+
+import PriorityQueue.PriorityQueue;
+import java.io.*;
+import java.util.*;
+
+public class Prim {
+    public static <V, L extends Number> Collection<? extends AbstractEdge<V, L>> minimumSpanningForest(Graph<V, L> graph) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
