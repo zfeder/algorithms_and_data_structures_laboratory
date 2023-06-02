f5fef6de966dc6a491fdc07edc9ed9af5bf3975a
Federico Filì <federico.fili@di.unito.it>
2023-06-02 19:28:40 +0200

    fix edge e aggiunti commenti

--- ex3-4/src/AbstractGraph/Edge.java
+++ ex3-4/src/AbstractGraph/Edge.java
@@ -1,24 +1,55 @@
 package AbstractGraph;
 
+/**
+ * Implementazione di un Arco in un Grafo
+ * @author Federico Filì
+ */
 public class Edge<V, L> implements AbstractEdge<V, L> {
-    private V start;
-    private V end;
-    private L label;
 
+    /**
+     * Nodo di partenza
+     */
+    private final V start;
+
+    /**
+     * Nodo di destinazione
+     */
+    private final V end;
+
+    /**
+     * Etichetta dell'arco
+     */
+    private final L label;
+
+    /**
+     * Costruttore per creare nuovo oggetto Edge
+     */
     public Edge(V start, V end, L label) {
         this.start = start;
         this.end = end;
         this.label = label;
     }
 
+    /**
+     * Funzione per restituire il nodo di partenza
+     * @return nodo di partenza
+     */
     public V getStart() {
         return start;
     }
 
+    /**
+     * Funzione per restituire il nodo di destinazione
+     * @return nodo di destinazione
+     */
     public V getEnd() {
         return end;
     }
 
+    /**
+     * Funzione per restituire l'etichetta dell'arco
+     * @return etichetta dell'arco
+     */
     public L getLabel() {
         return label;
     }
