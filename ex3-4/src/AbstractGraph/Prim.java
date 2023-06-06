a197fdba9ddc4b63b5fda7206189706f740af831
Federico Filì <federico.fili@di.unito.it>
2023-06-07 00:01:48 +0200

    correzioni sulla funzione di calcolo della foresta

--- ex3-4/src/AbstractGraph/Prim.java
+++ ex3-4/src/AbstractGraph/Prim.java
@@ -21,76 +21,62 @@ public class Prim {
      * successivi
      */
     public static <V, L extends Number> Collection<? extends AbstractEdge<V, L>> minimumSpanningForest(Graph<V, L> graph) {
-        // prim nodo che viene utlizzato come nodo di partenza
-        V source = graph.getNodes().iterator().next();
-        if (!graph.containsNode(source))
+        if(graph == null)
             return null;
 
-        // result rappresenta l'albero di copertura minimo
-        Graph<V, L> result = new Graph<V, L>(false, true);
+        // minimumForest rappresenta la foresta di copertura minima
+        Graph<V, L> minimumForest = new Graph<V, L>(false, true);
 
-        // visited per tenere traccia dei nodi visitati durante l'esecuzione dell'algoritmo
-        HashSet<V> visited = new HashSet<>();
+        // nodeVisited per tenere traccia dei nodi visitati
+        HashSet<V> nodeVisited = new HashSet<>();
 
-        // comparatore per confrontare gli archi in base all etichette
-        Comparator<Edge<V, L>> comparator = new Comparator<>() {
-            public int compare(Edge<V, L> o1, Edge<V, L> o2) {
-                if (o1.getLabel() == null && o2.getLabel() == null)
-                    return 0;
-                if (o1.getLabel() == null)
-                    return 1;
-                if (o2.getLabel() == null)
-                    return -1;
-                return Double.compare(o1.getLabel().doubleValue(), o2.getLabel().doubleValue());
-            }
-        };
+        // comparatore per confrontare gli archi in base all'etichetta
+        Comparator<Edge<V, L>> comparator = Comparator.comparingDouble(e -> e.getLabel().doubleValue());
 
-        // utilizzo della coda di priorità per selezionare gli archi con etichetta minima
+        // coda di priorità per selezionare gli archi con etichetta minima
         PriorityQueue<Edge<V, L>> priorityQueue = new PriorityQueue<>(comparator);
 
-        // cicla su su tutti i nodi vicini al nodo di partenza
-        for (V v : graph.getNeighbours(source)) {
-            L l = graph.getLabel(source, v);
-            if (l != null) {
-                // crea un nuovo arco con il nodo di partenza e viene inserito nella coda di priorità
-                priorityQueue.push(new Edge<>(source, v, l));
-            }
-        }
+        // iterazione sui tutti nodi del grafo
+        for (V node : graph.getNodes()) {
+            if (!nodeVisited.contains(node)) {
+                // selezione di un nodo non visitato come radice e aggiunto all'insieme per indicare che è stato visitato
+                nodeVisited.add(node);
 
-        // il nodo di partenza viene aggiunto al grafo result
-        result.addNode(source);
+                // aggiunta di archi adiacenti al nodo radice nella coda di priorità
+                for (V neighbor : graph.getNeighbours(node)) {
+                    L label = graph.getLabel(node, neighbor);
+                    if (label != null) {
+                        priorityQueue.push(new Edge<>(node, neighbor, label));
+                    }
+                }
 
-        // il nodo di partenza viene inserito nell'insieme dei nodi visitati
-        visited.add(source);
+                while (!priorityQueue.empty()) {
+                    // estrazione dell'arco con etichetta minima
+                    AbstractEdge<V, L> minEdge = priorityQueue.top();
+                    priorityQueue.pop();
 
-        // contatore di numero di archi aggiunti
-        int numEdges = 0;
+                    V start = minEdge.getStart();
+                    V end = minEdge.getEnd();
 
-        // viene estratto l'arco con etichetta minima e viene rimosso dalla coda
-        while (numEdges <= graph.numNodes() - 1 && !priorityQueue.empty()) {
-            Edge<V, L> minEdge = priorityQueue.top();
+                    // controlla se il nodo di destinazione è già stato visitato
+                    if (nodeVisited.contains(end)) {
+                        continue;
+                    }
 
-            priorityQueue.pop();
+                    // aggiunta del nodo di destinazione all'insieme dei nodi visitati
+                    nodeVisited.add(end);
 
-            // se il nodo di destinazione dell'arco non è stato visitato
-            if (!visited.contains(minEdge.getEnd())) {
-                V end = minEdge.getEnd();
-                L label = minEdge.getLabel();
+                    // aggiunta dell'arco al risultato
+                    minimumForest.addNode(start);
+                    minimumForest.addNode(end);
+                    minimumForest.addEdge(start, end, minEdge.getLabel());
 
-                result.addNode(end);
-                result.addEdge(minEdge.getStart(), end, label);
-
-                // viene aggiunto all'interno dei nodi visitati
-                visited.add(end);
-
-                numEdges++;
-
-                // viene eseguito un ciclo su tutti i vicini del nodo di destinazione
-                for (V v : graph.getNeighbours(end)) {
-                    L l = graph.getLabel(end, v);
-                    if (l != null && !visited.contains(v)) {
-                        // viene inserito un nuovo arco nella coda di priorità
-                        priorityQueue.push(new Edge<>(end, v, l));
+                    // aggiunta di archi adiacenti al nodo di destinazione nella coda di priorità
+                    for (V neighbor : graph.getNeighbours(end)) {
+                        L label = graph.getLabel(end, neighbor);
+                        if (label != null) {
+                            priorityQueue.push(new Edge<>(end, neighbor, label));
+                        }
                     }
                 }
             }
@@ -99,23 +85,20 @@ public class Prim {
         // Stampa il numero di nodi e archi calcolato
         System.out.println("**************************************************");
         System.out.println("*    Dati verificati all'interno di Prim         *");
-        System.out.println("*    Source: " + source + "                       *");
-        System.out.println("*    Numero di Nodi: " + result.numNodes() + "                       *");
-        System.out.println("*    Numero di Archi: " + result.numEdges() + "                      *");
+        System.out.println("*    Numero di Nodi: " + minimumForest.numNodes() + "                       *");
+        System.out.println("*    Numero di Archi: " + minimumForest.numEdges() + "                      *");
         System.out.println("**************************************************\n");
 
-        // viene restituita la collezione degli archi dell'albero
-        return result.getEdges();
+        // collezione di archi
+        return minimumForest.getEdges();
     }
 
     public static void main(String[] args) {
         Graph<String, Double> graph = new Graph<String, Double>(false, true);
         File file = new File("/Users/feder/Desktop/laboratorio-algoritmi-2022-2023/ex3-4/src/AbstractGraph/italian_dist_graph.csv");
-
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
-
             while ((line = br.readLine()) != null) {
                 String[] split = line.split(",");
                 graph.addNode(split[0]);
@@ -148,24 +131,26 @@ public class Prim {
         System.out.println("**************************************************");
         System.out.println("*    Altre informazioni                          *");
         System.out.println("*    Dimensione del Grafo dopo Prim: " + prim.size() + "       *");
-        System.out.format("*    Peso totale: %.2f km", sum/2000);
-        System.out.println("                    *");
+        System.out.format("*    Peso totale: %.3f km", sum/2000);
+        System.out.println("                   *");
         System.out.println("**************************************************\n");
         try {
-            //String fileOutput = args[1].substring(0, args[1].length() - 4) + "_output.csv";
+            List<AbstractEdge<String, Double>> sortedPrim = new ArrayList<>(prim);
+
+            Collections.sort(sortedPrim, (e1, e2) -> e1.toString().compareTo(e2.toString()));
+
             String fileOutput = "/Users/feder/Desktop/laboratorio-algoritmi-2022-2023/ex3-4/src/AbstractGraph/output.csv";
             FileWriter outputWriter = new FileWriter(fileOutput);
-
-            for (AbstractEdge<String, Double> e : prim) {
+            for (AbstractEdge<String, Double> e : sortedPrim) {
                 outputWriter.write(e.toString() + "\n");
             }
-
             outputWriter.close();
             System.out.println("Scrittura su file completata con successo.");
         } catch (IOException e) {
-            System.out.println("Errore duraente la scrittua del file");
+            System.out.println("Errore durante la scrittura del file");
             e.printStackTrace();
         }
     }
 
 }
+
